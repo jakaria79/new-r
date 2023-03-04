60e81ecf3a472e16796e8c25168b76758a0d41d6
jakaria79 <smjakaria79@gmail.com>
2023-03-04 17:25:26 +0600

    first commit

--- js/app.js
+++ js/app.js
@@ -1,33 +1,88 @@
-const loadUniverseHubs = async()=>{
-    const url = `https://openapi.programming-hero.com/api/ai/uHubs`;
-    const res = await fetch(url);
-    const data = await res.json();
-    
-    displayUniverseHubs(data.data);
+const loadCard = async(dataLimit)=>{
+const url = ` https://openapi.programming-hero.com/api/ai/tools`;
+const res = await fetch(url);
+const data = await res.json();
+displayCard(data.data.tools,dataLimit);
 }
 
-const displayUniverseHubs = universeHubs =>{
-const universeHubsContainer = document.getElementById('hubs-container'); 
-    universeHubsContainer.textContent = '';
-    universeHubs.tools.forEach(tool =>{
-      console.log(tool);
-  //   const linkContainer = document.createElement('div');
-  //   linkContainer.classList.add('col');
-  //   linkContainer.innerHTML = `
-  //   <div class="card h-100">
-  //   <img src="${tool.image}" class="card-img-top" alt="...">
-  //    <div class="card-body">
-  //     <h5 class="card-title">${tool.name}</h5>
-  //     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-  //    </div>
-  //   <div class="card-footer">
-  //     <small class="text-muted">${tool.published_in}</small>
-  //    </div>
-  //  </div>
-  // `;
-  })
-       universeHubsContainer.appendChild(linkContainer);
+const displayCard = (card,dataLimit) =>{
+const cardContainer = document.getElementById('hubs-container');
+     cardContainer.textContent = '';
+     const showAll = document.getElementById('show-all');
+     card = card.slice(0,6);
+     if (dataLimit && data.Length >6){
+         
+         showAll.classList.remove('d-none');
+     }
+     else {
+      showAll.classList.add();
+     }
+     
+      card.forEach(tool => {
+       const linkContainer = document.createElement('div');
+        linkContainer.classList.add('col');
+        linkContainer.innerHTML = `
+        <div class="card h-100">
+        <img src="${tool.image}" class="card-img-top" alt="...">
+         <div class="card-body">
+          <h5 class="card-title">${tool.name}</h5>
+          <p class="card-text">${tool.features}</p>
+         </div>
+        <div class="card-footer">
+      <div>
+        <small class="text-muted fw-bold">${tool.name}</small>
+      </div>
+        
+      <div class="d-flex justify-content-between">
+        <small class="text-muted">${tool.published_in}</small>
+        
+        <button onclick="cardDetails('${tool.id})" class="btn btn-primary"data-bs-toggle="modal" data-bs-target="#cardDetailsModal"><i class="fa-solid fa-arrow-right"></i></button>
+        
+  
+</button>
+      </div>
+         </div>
+       </div>
+      `;
+      cardContainer.appendChild(linkContainer);
+      });  
+    toggleSpinner(false);     
 }
 
-loadUniverseHubs();
+document.getElementById('btn-show-all').addEventListener('click',function(){
+  toggleSpinner (true);
+   loadCard(dataLimit);
+})
 
+const toggleSpinner = isLoading =>{
+  const loaderSection = document.getElementById('loader');
+  if(isLoading){
+    loaderSection.classList.remove('d-none');
+  }
+  else {
+    loaderSection.classList.add('d-none');
+  }
+}
+
+const loadCardDetails = async id =>{
+   try{
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+   const res = await fetch(url);
+   const data = await res.json();
+   }
+   catch (error){
+    console.log(error)
+}
+  //  displayCardDetails(data.data.tools);  
+  console.log(data.data);
+  
+
+  const displayCardDetails = card =>{
+    const CardTitle = document.getElementById('cardDetailsModalLabel');
+    CardTitle.innerText = card.name;
+    const cardDetails = document.getElementById('modal-details');
+    //  cardDetails.innerHTML
+  }
+}
+
+loadCard();
